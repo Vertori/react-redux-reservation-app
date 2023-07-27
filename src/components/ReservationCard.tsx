8a9ae439f5fc5c2c52d41057654c8d710f9712bd
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-27 12:02:15 +0200

    displaying reservation state items

---
+++ src/components/ReservationCard.tsx
@@ -0,0 +1,9 @@
+interface ReservationCardTypes {
+  name: string;
+}
+
+const ReservationCard = ({ name }: ReservationCardTypes) => {
+  return <div className="reservation-card-container">{name}</div>;
+};
+
+export default ReservationCard;
